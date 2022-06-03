440ab839012c34a05824e5968571d61e2017c15c
Casper-Hauberg <haubergcasper@gmail.com>
2022-06-03 14:44:23 +0200

    Removed old post method

--- app.js
+++ app.js
@@ -66,60 +66,6 @@ app.post(
   }
 );
 
-// app.post("/", (req, res) => {
-//   const { firstName, lastName, email, phoneNumber, password } = req.body;
-
-//   if (!firstName) {
-//     res.status(400).send("firstName is required.");
-//   }
-
-//   if (firstName.length < FIRST_NAME_MIN_LEN) {
-//     res
-//       .status(400)
-//       .send(`Firstname must be greater than ${FIRST_NAME_MIN_LEN} characters`);
-//   }
-
-//   if (firstName.length > FIRST_NAME_MAX_LEN) {
-//     res
-//       .status(400)
-//       .send(`Firstname must be smaller than ${FIRST_NAME_MAX_LEN} characters`);
-//   }
-
-//   if (!lastName) {
-//     res.status(400).send("lastName is required.");
-//   }
-
-//   if (lastName.length < LAST_NAME_MIN_LEN) {
-//     res
-//       .status(400)
-//       .send(`Lastname must be greater than ${LAST_NAME_MIN_LEN} characters`);
-//   }
-
-//   if (lastName.length > LAST_NAME_MAX_LEN) {
-//     res
-//       .status(400)
-//       .send(`Lastname must be smaller than ${LAST_NAME_MAX_LEN} characters`);
-//   }
-
-//   if (!email) {
-//     res.status(400).send("Email is required.");
-//   }
-
-//   if (!phoneNumber) {
-//     res.status(400).send("Phone number is required.");
-//   }
-
-//   if (phoneNumber.length !== PHONE_NUMBER_MAX_LEN) {
-//     res.status(400).send(`Phone number must be ${PHONE_NUMBER_MAX_LEN} digits`);
-//   }
-
-//   if (!password) {
-//     res.status(400).send("Password is required.");
-//   }
-
-//   res.status(200).send("User was succesfully uploaded");
-// });
-
 app.put("/", (req, res) => {
   res.send("Put request");
 });
